d518c5286a54f76763db0d53fed0624141051892
v.gargaun <gargaunveceslav@gmail.com>
2020-07-07 15:24:05 +0300

    comit

--- src/main/java/WorkVectors.java
+++ src/main/java/WorkVectors.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 public class WorkVectors {
     public static void main(String[] args) {
         JFrame window=new JFrame();
+
         final JTextField startPointX=new JTextField();
         final JTextField startPointY=new JTextField();
         final JTextField endPointX=new JTextField();
@@ -93,6 +94,7 @@ public class WorkVectors {
                 vector2.setY1(Double.parseDouble(b[5]));
                 vector2.setX2(Double.parseDouble(b[6]));
                 vector2.setY2(Double.parseDouble(b[7]));
+                JFrame window2=new JFrame();
 
                 Vector vector3 = vector1.sumVector(vector1,vector2);
                 startPointX3.setText(String.valueOf(vector3.getX1()));
@@ -100,6 +102,32 @@ public class WorkVectors {
                 endPointX3.setText(String.valueOf(vector3.getX2()));
                 endPointY3.setText(String.valueOf(vector3.getY2()));
 
+                JLabel l3,x1,x2,y1,y2;
+
+                l3=new JLabel("New Vector");
+                x1=new JLabel("Start Point X");
+                y1=new JLabel("Start Point Y");
+                x2=new JLabel("End point X");
+                y2=new JLabel("End point Y");
+                l3.setBounds(10, 115, 70, 30);
+
+                x1.setBounds(80,4, 90,30);
+                y1.setBounds(180,4, 90,30);
+                x2.setBounds(280,4, 90,30);
+                y2.setBounds(380,4, 90,30);
+                window2.add(x1);
+                window2.add(x2);
+                window2.add(y1);
+                window2.add(y2);
+                window2.add(l3);
+                window2.add(startPointX3);
+                window2.add(startPointY3);
+                window2.add(endPointX3);
+                window2.add(endPointY3);
+
+                window2.setLayout(null);
+                window2.setVisible(true);
+                window2.setSize(500,300);
             }
         });
 
@@ -115,6 +143,8 @@ public class WorkVectors {
                 b[6] = endPointX2.getText();
                 b[7] = endPointY2.getText();
 
+
+
                 vector1.setX1(Double.parseDouble(b[0]));
                 vector1.setY1(Double.parseDouble(b[1]));
                 vector1.setX2(Double.parseDouble(b[2]));
@@ -124,6 +154,7 @@ public class WorkVectors {
                 vector2.setY1(Double.parseDouble(b[5]));
                 vector2.setX2(Double.parseDouble(b[6]));
                 vector2.setY2(Double.parseDouble(b[7]));
+                JFrame window2=new JFrame();
 
                 Vector vector3 = vector1.minusVector(vector1,vector2);
                 startPointX3.setText(String.valueOf(vector3.getX1()));
@@ -131,6 +162,32 @@ public class WorkVectors {
                 endPointX3.setText(String.valueOf(vector3.getX2()));
                 endPointY3.setText(String.valueOf(vector3.getY2()));
 
+                JLabel l3,x1,x2,y1,y2;
+
+                l3=new JLabel("New Vector");
+                x1=new JLabel("Start Point X");
+                y1=new JLabel("Start Point Y");
+                x2=new JLabel("End point X");
+                y2=new JLabel("End point Y");
+                l3.setBounds(10, 115, 70, 30);
+
+                x1.setBounds(80,4, 90,30);
+                y1.setBounds(180,4, 90,30);
+                x2.setBounds(280,4, 90,30);
+                y2.setBounds(380,4, 90,30);
+                window2.add(x1);
+                window2.add(x2);
+                window2.add(y1);
+                window2.add(y2);
+                window2.add(l3);
+                window2.add(startPointX3);
+                window2.add(startPointY3);
+                window2.add(endPointX3);
+                window2.add(endPointY3);
+
+                window2.setLayout(null);
+                window2.setVisible(true);
+                window2.setSize(500,300);
             }
         });
 
@@ -142,7 +199,7 @@ public class WorkVectors {
         window.add(y1);
         window.add(y2);
         window.add(l2);
-        window.add(l3);
+
 
         window.add(bSuma);
         window.add(bMinus);
@@ -157,14 +214,13 @@ public class WorkVectors {
         window.add(endPointX2);
         window.add(endPointY2);
 
-        window.add(startPointX3);
-        window.add(startPointY3);
-        window.add(endPointX3);
-        window.add(endPointY3);
 
-        window.setSize(500,600);
+
+        window.setSize(500,300);
+
         window.setLayout(null);
 
         window.setVisible(true);
+
     }
 }
